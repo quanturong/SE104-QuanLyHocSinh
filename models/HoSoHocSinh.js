1bedeb3b54c14e2dfc6f1938a38f0ec396c7bfe8
LocK48 <321quangloc@gmail.com>
2025-12-11 20:06:45 +0700

    fix Subject-Filter

--- models/HoSoHocSinh.js
+++ models/HoSoHocSinh.js
@@ -1,6 +1,7 @@
 const { DataTypes } = require("sequelize");
 const sequelize = require("../config/db");
 const LopHoc = require("./LopHoc");
+const BangDiemMonHoc = require('./BangDiemMonHoc');
 
 const HoSoHocSinh = sequelize.define("HoSoHocSinh", {
   MaHocSinh: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
@@ -18,5 +19,6 @@ const HoSoHocSinh = sequelize.define("HoSoHocSinh", {
 
 LopHoc.hasMany(HoSoHocSinh, { foreignKey: "MaLop" });
 HoSoHocSinh.belongsTo(LopHoc, { foreignKey: "MaLop" });
+BangDiemMonHoc.belongsTo(HoSoHocSinh, { foreignKey: 'MaHocSinh' });
 
 module.exports = HoSoHocSinh;
